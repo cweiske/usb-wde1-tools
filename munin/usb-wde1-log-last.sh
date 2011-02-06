dff892e8db134c378ef449820bc2a1090cb47b92
Christian Weiske <cweiske@cweiske.de>
2011-02-06 22:38:07 +0100

    add run command

--- munin/usb-wde1-log-last.sh
+++ munin/usb-wde1-log-last.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 # logs the last line. usable via nohup on the server
 # FIXME: send RESET or INIT and M2
+# FIXME: use absolute path for log-single-line.sh
+#
+# run command: nohup ./usb-wde1-log-last.sh &
+#
 socat /dev/ttyUSB0,b9600 STDOUT\
  | ./log-single-line.sh /tmp/usb-wde1-last
