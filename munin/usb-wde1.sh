35c70aad5a0b5a43d82ff58892df20ccbbfd735a
Christian Weiske <cweiske@cweiske.de>
2011-02-06 12:39:19 +0100

    munin plugin config

--- munin/usb-wde1.sh
+++ munin/usb-wde1.sh
@@ -13,6 +13,7 @@ usb-wde1 -  Munin plugin to report usb-wde1 temperature and humidity data
 
  [usb-wde1]
   env.logfile /var/log/usb-wde1.log
+  env.host_name House
   env.sensor0 Living room
   env.sensor1 Kitchen
 
@@ -23,11 +24,11 @@ usb-wde1 -  Munin plugin to report usb-wde1 temperature and humidity data
 
 =cut
 
-# suggest, config
 # params:
 # - logfile
 # - sensor names
-# - type (temperature / humidity
+# - type (temperature / humidity)
+# - hostname
 
 if [ "$1" = "autoconf" ]; then
     if [ -r "$logfile" ]; then
@@ -36,10 +37,32 @@ if [ "$1" = "autoconf" ]; then
         echo "no (Logfile \"$logfile\" does not exist)"
     fi
     exit 0
-fi
 
-if [ "$1" = "suggest" ]; then
+elif [ "$1" = "suggest" ]; then
     echo temperature
     echo humidity
     exit 0
+fi
+
+
+#load variables
+TYPE=`basename $0 | sed 's/^.*_//g'`
+
+
+if [ "$1" = "config" ]; then
+    if [ "$host_name" != "" ]; then
+        echo "host_name $host_name"
+    fi
+    if [ "$TYPE" = "temperature" ]; then
+        echo 'graph_title Temperature'
+        echo 'graph_args --base 1000 --lower-limit -30 --upper-limit 60'
+        echo 'graph_vlabel Temperature'
+    else
+        echo 'graph_title Humidity'
+        echo 'graph_args --base 1000 --lower-limit 0 --upper-limit 100'
+        echo 'graph_vlabel Humidity'
+    fi
+    echo 'graph_scale no'
+    echo 'graph_category sensors'
+    #FIXME: warning/critical values
 fi
