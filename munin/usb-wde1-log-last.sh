fc391abfe0a9a02162e6bbe293859a04c1eee26a
Christian Weiske <cweiske@cweiske.de>
2011-02-08 08:01:29 +0100

    add license and author

--- munin/usb-wde1-log-last.sh
+++ munin/usb-wde1-log-last.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 # logs the last line. usable via nohup on the server
+#
+# run this script as follows:
+# $ nohup ./usb-wde1-log-last.sh &
+#
+# License: http://www.gnu.org/licenses/agpl.html AGPL
+# Author: Christian Weiske <cweiske@cweiske.de>
+#
 # FIXME: send RESET or INIT and M2
 # FIXME: use absolute path for log-single-line.sh
-#
-# run command: nohup ./usb-wde1-log-last.sh &
 #
 socat /dev/ttyUSB0,b9600 STDOUT\
  | ./log-single-line.sh /tmp/usb-wde1-last
